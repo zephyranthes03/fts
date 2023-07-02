cf8b1ee3f12990c5bf68eeacdebf93c4f4aa0664
YJ chong <zephyranthes03@gmail.com>
2023-07-01 20:33:23 -0600

    Fix orm ports

--- ORM-comment-service/app/main.py
+++ ORM-comment-service/app/main.py
@@ -1,3 +1,3 @@
 import uvicorn
 if __name__ == "__main__":
-    uvicorn.run("server.app:app", host="0.0.0.0", port=8008, reload=True)
+    uvicorn.run("server.app:app", host="0.0.0.0", port=8009, reload=True)
