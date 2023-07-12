f70b194b4e66972f136072ab67519616636f7d5b
Yongjin Chong <zephyranthes03@gmail.com>
2023-07-11 19:39:17 -0600

    Update template

--- community-service/app/server/schemas/community.py
+++ community-service/app/server/schemas/community.py
@@ -3,7 +3,6 @@ from typing import Optional
 
 from pydantic import BaseModel, Field
 from app.server.schemas.common import ErrorResponseModel, ResponseModel
-from app.config.config import settings
 
 class Community_schema(BaseModel):  
     id: str = Field(default_factory=uuid.uuid4, alias="_id")
@@ -12,8 +11,8 @@ class Community_schema(BaseModel):
     date: str = Field(...)
     boards: str = Field(...)
     manager: str = Field(...)
-    image_file = Field(...)
-    content = Field(...)   
+    image_file: str = Field(...)
+    content: str = Field(...)   
 
     class Config:
         schema_extra = {
@@ -23,7 +22,8 @@ class Community_schema(BaseModel):
                 "date": "2017-10-13T10:53:53Z",
                 "boards": "{100:'intro',}",
                 "manager": "admin",
-                "image_file": "http://localhost:8005/community/image/066de609-b04a-4b30-b46c-32537c7f1f6e.jpg"
+                "image_file": "http://localhost:8005/community/image/066de609-b04a-4b30-b46c-32537c7f1f6e.jpg",
+                "content": "contents"
             }
         }
 
@@ -33,8 +33,8 @@ class Update_community_schema(BaseModel):
     date: str = Field(...)
     boards: str = Field(...)
     manager: str = Field(...)
-    image_file = Field(...)
-    content = Field(...)   
+    image_file: str = Field(...)
+    content: str = Field(...)   
 
     class Config:
         schema_extra = {
@@ -44,7 +44,8 @@ class Update_community_schema(BaseModel):
                 "date": "2017-10-13T10:53:53Z",
                 "boards": "{100:'intro',}",
                 "manager": "admin",
-                "image_file": "http://localhost:8005/community/image/066de609-b04a-4b30-b46c-32537c7f1f6e.jpg"
+                "image_file": "http://localhost:8005/community/image/066de609-b04a-4b30-b46c-32537c7f1f6e.jpg",
+                "content": "contents"
             }
         }
 
