27b7cd96451f600101f88fcd877598f7a7409b5d
YJ chong <zephyranthes03@gmail.com>
2023-07-15 00:23:00 -0600

    Fix error checking mongoDB status

--- ORM-symptom-service/app/server/app.py
+++ ORM-symptom-service/app/server/app.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
+from time import sleep
 from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
 
@@ -37,13 +38,17 @@ app = start_application()
 def startup_db_client():
     mongodb_flag = False
     mongodb_delay = 30
+    client = None
     while mongodb_flag == False:
         client = MongoClient(settings.DATABASE_URI, serverSelectionTimeoutMS=10, connectTimeoutMS=20000)
         try:
-            info = client.server_info() # Forces a call.
+            mongodb_flag = client.server_info() # Forces a call.
         except ServerSelectionTimeoutError:
             print(f"MongoDB is not Ready yet try again {mongodb_delay} seconds later", flush=True)
+        if mongodb_flag == False:
+            sleep(mongodb_delay)
     client.close()
+    print(f"MongoDB is Ready", flush=True)
 
     # If connection create a new one with serverSelectionTimeoutMS=30000
     app.mongodb_client = MongoClient(settings.DATABASE_URI)
