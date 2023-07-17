16da78af27e28db8deb9354a732613b84305182d
Yongjin Chong <zephyranthes03@gmail.com>
2023-07-17 17:56:50 -0600

    Update prerequirement check routine for symptom, community services

--- symptom-service/app/server/app.py
+++ symptom-service/app/server/app.py
@@ -1,4 +1,6 @@
 import os
+import httpx
+
 from time import sleep
 from redis import Redis
 
@@ -25,27 +27,34 @@ async def read_root():
 @app.on_event("startup")
 async def startup_client():
     redis_flag = False
-    mongodb_flag = False
     redis_delay = 15
-    mongodb_flag = 30
     while redis_flag == False:
         redis_host = os.getenv("REDIS_HOST","redis")
         r = Redis(redis_host, socket_connect_timeout=1) # short timeout for the test
         redis_flag = r.ping()
         if redis_flag == False:
+            print(f"Redis is not Ready yet try again {redis_delay} seconds later", flush=True)
             sleep(redis_delay)
 
-    while redis_flag == False:
-        redis_host = os.getenv("REDIS_HOST","redis")
-        r = Redis(redis_host, socket_connect_timeout=1) # short timeout for the test
-        redis_flag = r.ping()
-        if redis_flag == False:
-            sleep(redis_delay)
+    metacommunity_delay = 30
+    metacommunity_flag = False
+    response = None
+    while metacommunity_flag == False:
+        async with httpx.AsyncClient() as client:
+            metacommunity_host = os.getenv("ORM_SYMPTOM_SERVICE", 
+                                        "http://orm-symptom-service:8004")
+            try:            
+                response = await client.get(f'{metacommunity_host}',timeout=3)
+            except httpx.HTTPError as exc:
+                print(f"HTTP error: {exc}")
+
+            if response is not None:
+                print(response.status_code, flush=True)
+                metacommunity_flag = True if response.status_code == 200 else False
+            else:
+                print(f"Service is not Ready yet try again {metacommunity_delay} seconds later", flush=True)
+                sleep(metacommunity_delay)
 
 
-
-
-    pass
-    #await load_symptom_indexes()
     
 
