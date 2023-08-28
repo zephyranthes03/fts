2794fa92573e34da42df99e6ee477e31c64c8012
YJ chong <zephyranthes03@gmail.com>
2023-08-28 16:20:30 -0600

    Update message data type

---
+++ orm-services/distribute.sh
@@ -0,0 +1,87 @@
+# routes
+
+cp ./app/server/routes/diagnosis.py ../ORM-symptom-service/app/server/routes/diagnosis.py
+cp ./app/server/routes/disease.py ../ORM-symptom-service/app/server/routes/disease.py
+cp ./app/server/routes/symptom_index.py ../ORM-symptom-service/app/server/routes/symptom_index.py
+
+cp ./app/server/routes/post.py ../ORM-post-service/app/server/routes/post.py
+
+cp ./app/server/routes/board.py ../ORM-metacommunity-service/app/server/routes/board.py
+cp ./app/server/routes/community.py ../ORM-metacommunity-service/app/server/routes/community.py
+
+cp ./app/server/routes/message.py ../ORM-message-service/app/server/routes/message.py
+
+cp ./app/server/routes/application.py ../ORM-member-service/app/server/routes/application.py
+cp ./app/server/routes/member.py ../ORM-member-service/app/server/routes/member.py
+
+cp  ./app/server/routes/log_community.py ../ORM-log-service/app/server/routes/log_community.py
+
+cp ./app/server/routes/comment.py ../ORM-comment-service/app/server/routes/comment.py
+
+#schemas
+
+cp ./app/server/schemas/diagnosis.py ../ORM-symptom-service/app/server/schemas/diagnosis.py
+cp ./app/server/schemas/disease.py ../ORM-symptom-service/app/server/schemas/disease.py
+cp ./app/server/schemas/symptom_index.py ../ORM-symptom-service/app/server/schemas/symptom_index.py
+
+cp ./app/server/schemas/post.py ../ORM-post-service/app/server/schemas/post.py
+
+cp ./app/server/schemas/board.py ../ORM-metacommunity-service/app/server/schemas/board.py
+cp ./app/server/schemas/community.py ../ORM-metacommunity-service/app/server/schemas/community.py
+
+cp ./app/server/schemas/message.py ../ORM-message-service/app/server/schemas/message.py
+
+cp ./app/server/schemas/application.py ../ORM-member-service/app/server/schemas/application.py
+cp ./app/server/schemas/member.py ../ORM-member-service/app/server/schemas/member.py
+
+cp ./app/server/schemas/log_community.py ../ORM-log-service/app/server/schemas/log_community.py
+
+cp ./app/server/schemas/comment.py ../ORM-comment-service/app/server/schemas/comment.py
+
+
+#database
+
+cp ./app/server/databases/diagnosis.py ../ORM-symptom-service/app/server/databases/diagnosis.py
+cp ./app/server/databases/disease.py ../ORM-symptom-service/app/server/databases/disease.py
+cp ./app/server/databases/symptom_index.py ../ORM-symptom-service/app/server/databases/symptom_index.py
+
+cp ./app/server/databases/post.py ../ORM-post-service/app/server/databases/post.py
+
+cp ./app/server/databases/board.py ../ORM-metacommunity-service/app/server/databases/board.py
+cp ./app/server/databases/community.py ../ORM-metacommunity-service/app/server/databases/community.py
+
+cp ./app/server/databases/message.py ../ORM-message-service/app/server/databases/message.py
+
+cp ./app/server/databases/application.py ../ORM-member-service/app/server/databases/application.py
+cp ./app/server/databases/member.py ../ORM-member-service/app/server/databases/member.py
+
+cp ./app/server/databases/log_community.py ../ORM-log-service/app/server/databases/log_community.py
+
+cp ./app/server/databases/comment.py ../ORM-comment-service/app/server/databases/comment.py
+
+
+#database/models
+
+cp ./app/server/databases/models/diagnosis.py ../ORM-symptom-service/app/server/databases/models/diagnosis.py
+cp ./app/server/databases/models/disease.py ../ORM-symptom-service/app/server/databases/models/disease.py
+cp ./app/server/databases/models/symptom_index.py ../ORM-symptom-service/app/server/databases/models/symptom_index.py
+
+cp ./app/server/databases/models/post.py ../ORM-post-service/app/server/databases/models/post.py
+
+cp ./app/server/databases/models/board.py ../ORM-metacommunity-service/app/server/databases/models/board.py
+cp ./app/server/databases/models/community.py ../ORM-metacommunity-service/app/server/databases/models/community.py
+
+cp ./app/server/databases/models/message.py ../ORM-message-service/app/server/databases/models/message.py
+
+cp ./app/server/databases/models/application.py ../ORM-member-service/app/server/databases/models/application.py
+cp ./app/server/databases/models/member.py ../ORM-member-service/app/server/databases/models/member.py
+
+cp ./app/server/databases/models/log_community.py ../ORM-log-service/app/server/databases/models/log_community.py
+
+cp ./app/server/databases/models/comment.py ../ORM-comment-service/app/server/databases/models/comment.py
+
+
+
+
+
+
